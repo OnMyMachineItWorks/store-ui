f693e92d87b4d6a6dd51496fbf7a413693da5799
OnMyMachineItWorks <124904013+OnMyMachineItWorks@users.noreply.github.com>
2023-12-22 00:09:53 +0530

    verified api integration

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script setup lang="ts">
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { ref, onMounted } from 'vue';
+
+onMounted(async () => {
+  await testApiCall();
+})
+
+async function testApiCall() {
+  const response = await fetch('http://localhost:5113/Store');
+  const data = await response.json();
+
+}
 
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // devtool: 'source-map',
+  devServer: {
+    proxy: 'https://localhost:8080/',
+    }
 })
